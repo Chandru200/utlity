a37d817ba98c2a7de6f742a0152dff0dace152eb
Chandru200 <chandru160821@gmail.com>
2023-10-11 14:34:02 +0530

    website monitoring - chrome work done

--- apiwrapper.js
+++ apiwrapper.js
@@ -1,4 +1,4 @@
-function getRequest(url, sucessCallback, errorCallback) {
+function getRequest(url, sucessCallback, errorCallback, stopSync) {
   let fetchRes = fetch("http://127.0.0.1:8000/" + url, {
     method: "GET",
     headers: {
@@ -23,6 +23,7 @@ function getRequest(url, sucessCallback, errorCallback) {
         });
       } else {
         sucessCallback(response);
+        !stopSync && syncData();
       }
     })
     .catch((error) => {
@@ -30,7 +31,7 @@ function getRequest(url, sucessCallback, errorCallback) {
     });
 }
 
-function postRequest(url, data, sucessCallback, errorCallback) {
+function postRequest(url, data, sucessCallback, errorCallback, stopSync) {
   let fetchRes = fetch("http://127.0.0.1:8000/" + url, {
     method: "POST",
     headers: {
@@ -56,9 +57,51 @@ function postRequest(url, data, sucessCallback, errorCallback) {
         });
       } else {
         sucessCallback(response);
+        !stopSync && syncData();
       }
     })
     .catch((error) => {
       errorCallback(error);
     });
+}
+function syncData() {
+  chrome.storage.local.get(["view_time", "canChangePending"]).then((result) => {
+    if (result.view_time && result.canChangePending) {
+      chrome.storage.local
+        .set({ pendingSyncData: result.view_time })
+        .then(() => {
+          chrome.storage.local.set({ canChangePending: false });
+          chrome.storage.local.set({ view_time: {} });
+          chrome.storage.local.get(["pendingSyncData"]).then((result) => {
+            postRequest(
+              "sync_viewed_time",
+              { view_time: result.pendingSyncData },
+              () => {
+                chrome.storage.local.set({ pendingSyncData: {} });
+                chrome.storage.local.set({ canChangePending: true });
+              },
+              (error) => {
+                console.log("error");
+              },
+              true
+            );
+          });
+        });
+    } else if (!result.canChangePending) {
+      chrome.storage.local.get(["pendingSyncData"]).then((result) => {
+        postRequest(
+          "sync_viewed_time",
+          { view_time: result.pendingSyncData },
+          () => {
+            chrome.storage.local.set({ pendingSyncData: {} });
+            chrome.storage.local.set({ canChangePending: true });
+          },
+          (error) => {
+            console.log("error");
+          },
+          true
+        );
+      });
+    }
+  });
 }
