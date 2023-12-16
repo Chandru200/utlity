6bc62c6945e4251581b05dcfa7c3e013e15e5114
Chandru P <chandru20001@gmail.com>
2023-12-16 18:34:46 +0530

    google calendar api integrations

--- popup/index.js
+++ popup/index.js
@@ -13,6 +13,48 @@ function sendMessageToAllTabs(message, data) {
     }
   });
 }
+window.onload = function() {
+  document.querySelector('button').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      console.log(token);
+      let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+      };
+        fetch(
+          'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+          init)
+          .then((response) => response.json())
+          .then(function(data) {
+
+            console.log(data.items)
+          });
+
+
+          let accessToken = token;
+          let calendarId = 'chandru20001@gmail.com';
+          fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
+            headers: {
+              Authorization: 'Bearer ' + accessToken,
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data.items); 
+            })
+            .catch((error) => {
+              console.error('Error fetching events:', error);
+            });
+
+          
+    });
+  });
+};
 
 chrome.storage.local.get(["canShowAppButton"]).then((result) => {
   document.getElementById("showcontentbutton").checked =
